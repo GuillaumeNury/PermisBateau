39730f9370d686ba291fef161ea27b6a6c62257c
Guillaume Nury <nury.guillaume@gmail.com>
2020-03-10 13:21:16 +0100

    [fix] routing

--- src/App.js
+++ src/App.js
@@ -5,16 +5,41 @@ import ChallengePage from './pages/ChallengePage';
 import IndexPage from './pages/IndexPage';
 import LevelBuilderPage from './pages/LevelBuilderPage';
 
-function App() {
-  const route = window.location.hash.slice(1);
 
-  switch (route)
-  {
-    case '/challenge': return <ChallengePage />
-    case '/all': return <AllLevelPage />
-    case '/level-builder': return <LevelBuilderPage />
-    default: return <IndexPage />
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        route: this._getCurrentRoute(),
+    }
+
+    this.hashChangeHandler = () => this._onHachChange();
+  }
+
+  componentDidMount() {
+      window.addEventListener('hashchange', this.hashChangeHandler);
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('hashchange', this.hashChangeHandler);
+  }
+
+  _onHachChange() {
+      this.setState({ route: this._getCurrentRoute(), });
+  }
+
+  _getCurrentRoute() {
+      return window.location.hash.slice(1);
+  }
+
+  render() {
+    switch (this.state.route)
+    {
+      case '/challenge': return <ChallengePage />
+      case '/all': return <AllLevelPage />
+      case '/level-builder': return <LevelBuilderPage />
+      default: return <IndexPage />
+    }
   }
 }
-
-export default App;
--- src/pages/AllLevelPage.js
+++ src/pages/AllLevelPage.js
@@ -6,6 +6,7 @@ export default function AllLevelPage()
 {
     return (
         <div style={{ display: "inline-flex", flexWrap: "wrap" }}>
+            <h3>{levels.length} niveaux</h3>
             {levels.map((lvl, idx) => <div key={idx} style={{ border: "1px solid #666", flexGrow: 1 }}>
                 <Challenge level={lvl}>
                     <div style={{ textAlign: 'center' }}>{lvl.solution}</div>
